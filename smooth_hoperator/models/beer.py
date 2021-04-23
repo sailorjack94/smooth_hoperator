a8dc788e651381a22971c6783d8a0b7f77e589e8
sailorj94 <jackniall9@gmail.com>
2021-04-23 13:38:44 +0100

    Classes defined for MVP

---
+++ smooth_hoperator/models/beer.py
@@ -0,0 +1,9 @@
+class Beer:
+    def __init__(self, name, description, style, buy_price, sell_price, brewer, id=None):
+        self.name = name
+        self.description = description
+        self.style = style
+        self.buy_price = buy_price
+        self.sell_price = sell_price
+        self.brewer = brewer
+        self.id = id
---
+++ smooth_hoperator/models/brewer.py
@@ -0,0 +1,5 @@
+class Brewer:
+    def __init__(self, name, description, id=None):
+        self.name = name
+        self.description = description
+        self.id = id
