c5c82e47a00da7e9b7094521e8f465058375a491
sailorj94 <jackniall9@gmail.com>
2021-04-25 18:26:28 +0100

    Forms to add beer/brewer added and working

--- smooth_hoperator/controllers/beer_controller.py
+++ smooth_hoperator/controllers/beer_controller.py
@@ -18,3 +18,27 @@ def beers():
 def show_beer(id):
     beer = beer_repository.select(id)
     return render_template('beers/show.html', beer = beer)
+
+@beer_blueprint.route('/beers/new')
+def new_beer():
+    beers = beer_repository.select_all()
+    brewers = brewer_repository.select_all()
+    return render_template("beers/new.html" ,all_brewers = brewers, all_beers = beers)
+
+@beer_blueprint.route('/beers', methods = ["POST"])
+def add_new_beer():
+    name    = request.form['name']
+    description = request.form['description']
+    style = request.form['style']
+    stock = request.form['stock']
+    buy_price = request.form['buy_price']
+    sell_price = request.form['sell_price']
+    brewer  = brewer_repository.select(request.form['brewer_id'])
+    beer = Beer(name, description, style, stock, buy_price, sell_price, brewer)
+    beer_repository.save(beer)
+    return redirect('/beers') 
+
+@beer_blueprint.route('/stock')
+def stock():
+    beers = beer_repository.select_all()
+    return render_template('beers/stock.html', all_beers = beers)
